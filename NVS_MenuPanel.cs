d1528572931c23b2192bd473cc83f4e49d69bae5
Md Zunaed A. Tanim <56795901+zunaed-tanim@users.noreply.github.com>
2019-10-30 15:31:01 +0600

    MainMenu manager scripts

---
+++ NVS_MenuPanel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class NVS_MenuPanel : MonoBehaviour {
+
+    public GameObject loadingScreen;
+    public Slider slider;
+    public TextMeshProUGUI progressText;
+
+    public void StartGame(int sceneIndex)
+    {
+        Time.timeScale = 1f;
+        //SceneManager.LoadScene("NVS_Hangar");
+        StartCoroutine(LoadSceneAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadSceneAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progressText.text = "Loading " + (int)progress * 100f + "%";
+
+            yield return null;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
